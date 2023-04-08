44a80711b7983ecc6d7e672ea3efd71f3378c02c
Luis Gutiérrez <lagm777@gmail.com>
2023-04-08 07:16:39 -0600

    Adding useState

---
+++ projects/00-hola-mundo/src/InitialComponent.jsx
@@ -0,0 +1,24 @@
+import { useState, useCallback } from "react";
+
+function CountButton() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = (value) => {
+    if (value) {
+      setCount(count + 1);
+    } else {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>{count}</p>
+      <button onClick={() => handleClick(true)}>Sumar</button>
+      <button onClick={() => handleClick(false)}>Restar</button>
+    </div>
+  );
+}
+
+export default CountButton;
