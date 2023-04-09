a91d6b905d0171002c656dd717d809dab2c95f8e
Luis Gutiérrez <lagm777@gmail.com>
2023-04-09 08:58:46 -0600

    adding 3rd project

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App () {
@@ -10,7 +10,7 @@ function App () {
     <div className='App'>
       <div>
         <a href='https://vitejs.dev'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
+          {/* <img src={viteLogo} className='logo' alt='Vite logo' /> */}
         </a>
         <a href='https://reactjs.org'>
           <img src={reactLogo} className='logo react' alt='React logo' />
